dd62dc9fbc6e84e064e2619ab63037571d52f310
Brad Pizzimenti <brad@gennakersystems.com>
2016-03-27 20:53:22 -0700

    display food item details using a 'zippy' method

--- app/food-display.component.ts
+++ app/food-display.component.ts
@@ -7,14 +7,33 @@ import {Food} from './food.model'
   template: `
   <h3
     (click)="foodClicked(food)"
-  >{{food.name}}</h3>
+    [class.selected]="food"
+  >{{food.name}}
+  </h3>
+
+  <div class="zippy">
+    <div class="zippy__title" (click)="toggle()">
+      <h4>{{ visible ? '&#x025BE;' : '&#x025B8;' }} Details</h4>
+    </div>
+    <div class="zippy__content" [hidden]="!visible">
+      This is some content.
+    </div>
+  </div>
+
   `
 })
 
 export class FoodDisplayComponent {
   public food: Food;
-  constructor() {}
+  public visible: boolean;
+
+  constructor() {
+    this.visible = false;
+  }
   foodClicked(clickedFood: Food): void {
     console.log("foodClicked method:", clickedFood);
+  }
+  toggle() {
+    this.visible = !this.visible;
   }
 }
